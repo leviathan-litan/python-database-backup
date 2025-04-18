9a156f4ebe92287f1c747cc92f7c68b16f06735a
yihuan.yu <yihuan.yu@aesc-group.com>
2025-04-18 09:22:24 +0800

    feat: 添加MySQL数据库备份脚本和配置文件
    - 新增mysql_database__backup.py脚本，支持通过mysqldump备份MySQL数据库
    - 添加mysql_database__backup.yml配置文件，用于配置备份路径和数据库信息
    - 更新README.md，补充Python版本、依赖包和操作系统要求
    - 更新.gitignore，忽略MacOS系统文件

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # python-database-backup
 Python：数据库备份脚本
+
+## Python 版本
+Python 3.6.8+
+
+## 依赖包
+```
+pip install pyyaml
+pip install arrow
+```
+
+## 操作系统
+- Linux
+Linux 本地 需要安装 mysql-client，有 mysqldump
+```
+[root@mysql01 ~]# mysqldump -V
+mysqldump  Ver 9.1.0-commercial for Linux on x86_64 (MySQL Enterprise Server - Commercial)
+[root@mysql01 ~]# 
+```
---
+++ mysql_database__backup.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+
+# ==================================
+# 模块
+# ==================================
+
+# ))))))))))))))))))))))))))))
+# OS
+# ))))))))))))))))))))))))))))
+
+import os, sys
+
+# ))))))))))))))))))))))))))))
+# Time
+# ))))))))))))))))))))))))))))
+
+from datetime import datetime
+
+# - pip install arrow
+import arrow
+
+# ))))))))))))))))))))))))))))
+# YAML
+# ))))))))))))))))))))))))))))
+
+# - pip install pyyaml
+import yaml
+
+# ))))))))))))))))))))))))))))
+# Progress Bar
+# ))))))))))))))))))))))))))))
+
+# - pip install tqdm
+# from tqdm import tqdm
+
+# ==================================
+# YAML
+# ==================================
+
+# - 加载 YAML 文件
+def yaml_load_file(config_file):
+
+    read_file = open(
+        file=config_file,
+        mode='r',
+        encoding='utf-8'
+    )
+    read_file_content = read_file.read()
+    read_file.close()
+
+    # yaml_data = yaml.load(
+    #     stream=read_file_content,
+    #     Loader=yaml.FullLoader
+    # )
+    yaml_data = yaml.safe_load(
+        read_file_content
+    )
+
+    return yaml_data
+
+def yaml_get_value(config_file_obj, variable_path, split_char='.'):
+    
+    data_target = config_file_obj
+
+    for path_item in variable_path.split(split_char):
+        if not path_item.isdigit():
+            data_target = data_target[str(path_item)]
+        else:
+            data_target = data_target[int(path_item)]
+    
+    return data_target
+
+# ==================================
+# File and Directory
+# ==================================
+
+# 创建目录
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        # print(f"目标路径【{path}】已存在")
+        pass
+    except OSError as err:
+        print(err)
+
+# ==================================
+# Date and Time
+# ==================================
+
+def display_current_datetime(message):
+    print("")
+    print(f"%%%%%%%%%%%%%%%%%%%%%%%%%% [{arrow.now()}] {message}")
+    print("")
+
+# ==================================
+# Database: MySQL
+# ==================================
+
+
+# ==================================
+# 变量
+# ==================================
+
+# 配置文件
+config_file = "mysql_database__backup.yml"
+config_file_obj = yaml_load_file(
+    config_file=config_file
+)
+
+backup_meta_dir_backup_base = yaml_get_value(
+    config_file_obj=config_file_obj,
+    variable_path="backup_meta.dir_backup_base"
+)
+
+backup_list = yaml_get_value(
+    config_file_obj=config_file_obj,
+    variable_path="backup_list"
+)
+
+# ==================================
+# 运行
+# ==================================
+
+if backup_meta_dir_backup_base:
+    make_sure_path_exists(backup_meta_dir_backup_base)
+
+# =============================== 开始
+display_current_datetime(
+    message="执行【开始】"
+)
+
+# =============================== 执行
+
+backup_list_item_cursor = 1
+for backup_list_item in backup_list:
+
+    # 开始阶段
+    print(f"%%%%%%%%%%%%%%%%%%%%%%%%%% {backup_list_item_cursor}")
+
+    # 显示
+    # 原始信息
+    # print(backup_list_item)
+    # print(f"------------")
+
+    # 变量
+    # -- 时间信息
+    current_date_YYYYMMDD = arrow.now().format('YYYYMMDD')
+    current_date_YYYYMMDD_HHmmss = arrow.now().format('YYYYMMDD_HHmmss')
+
+    # 时间
+    print(f"当前时间【{current_date_YYYYMMDD}】")
+    print(f"当前时间【{current_date_YYYYMMDD_HHmmss}】")
+    print(f"------------")
+
+    # 变量
+    
+    # -- 数据库信息
+    vender = backup_list_item['vender']
+    host = backup_list_item['host']
+    port = backup_list_item['port']
+    db_name = backup_list_item['db_name']
+    user = backup_list_item['user']
+    password = backup_list_item['password']
+    dir_backup_base = backup_list_item['dir_backup_base']
+
+    # 当前数据库的备份起始目录（dir_backup_base）
+    # 如果没有指定，则使用全局默认的备份起始目录
+    if dir_backup_base == "":
+        dir_backup_base = backup_meta_dir_backup_base
+
+    # -- 备份文件所在的目录
+    dir_backup_file = f"{dir_backup_base}/{vender}/{host}/{current_date_YYYYMMDD}"
+
+    if dir_backup_file:
+        make_sure_path_exists(dir_backup_file)
+
+    # 格式化输出
+    print(f"数据库类型：{vender}")
+    print(f"备份文件所在目录：{dir_backup_file}")
+    
+    # -- 备份文件的名称
+    file_backup = ""
+    
+    # 根据不同的 数据库类型（vender） 执行不同的备份策略
+    # Python 3.10 之前：if ... elif ... else
+    # Python 3.10 之后：match ... case
+
+    if vender == "mysql":
+
+        # -- 备份文件名称的后缀
+        backup_file_suffix = "sql"
+        # -- 备份文件的名称
+        file_backup = f"db_backup_file__{ vender }_{ host }_{ db_name }_{ current_date_YYYYMMDD_HHmmss }.{backup_file_suffix}"
+        file_backup_log = f"db_backup_file__{ vender }_{ host }_{ db_name }_{ current_date_YYYYMMDD_HHmmss }.log"
+
+        # -- 备份文件完整路径
+        full_path_backup_file = f"{dir_backup_file}/{file_backup}"
+
+        # 显示
+        print(f"------------")
+        print(f"备份文件的名称：{file_backup}")
+        print(f"备份文件的完整路径：{full_path_backup_file}")
+        print(f"------------")
+
+        # 备份的命令 / 预览
+        command_backup = f"mysqldump -h {host} -u {user} -p{password} -P{port} --single-transaction --quick --databases {db_name} | gzip > {full_path_backup_file}.gz"
+
+        print(f"备份语句【{command_backup}】")
+        print(f"------------")
+        
+        # 备份的命令 / 执行
+        exit_code = os.system(command_backup)
+        
+        if exit_code == 0:
+            display_current_datetime(
+                message="数据库备份【成功】"
+            )
+        else:
+            display_current_datetime(
+                message="数据库备份【失败】"
+            )
+        
+    # 结束阶段
+    print()
+
+    backup_list_item_cursor += 1
+
+# =============================== 结束
+display_current_datetime(
+    message="执行【完成】"
+)
+
+# ==================================
+# 结束
+# ==================================
